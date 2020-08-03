7ec0a9ad61c31811e1899d9fe1454043ef5d8675
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-08-04 03:07:11 +0800

    update pretrained model

--- model/__init__.py
+++ model/__init__.py
@@ -147,10 +147,13 @@ def get_model(model_name="se_resnext50_32x4d", num_classes=101, pretrained="imag
     dim_feats = model.last_linear.in_features
     model.last_linear = nn.Linear(dim_feats, num_classes)
     model.avg_pool = nn.AdaptiveAvgPool2d(1)
+    model = nn.Sequential(
+        model,
+        nn.Softmax(dim = 1))
     return model
 
 
 if __name__ == '__main__':
-    model = ThinAge()
+    model = get_model()
     # torch.save(model, './checkpoint/ThinAge.pt')
     print(model)
